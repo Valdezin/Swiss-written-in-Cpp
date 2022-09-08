2edafb1de98a1b17247ab5dabd6d2d8ae96a55ad
Valdezin <ops@vldz.ga>
2022-09-08 10:17:21 +0800

    Added Simp(le) Calculator

--- main.cpp
+++ main.cpp
@@ -3,27 +3,56 @@
 using namespace std;
 
 int main(){
-  cout << "SWISS Military VLDZ\n\n Please choose a function\n\nType 1 to print Author's Name\nType 2 for Addition\n";
+  cout << "SWISS Military VLDZ\n\n Please choose a function\n\nType 1 to print Author's Name\nType 2 for Simp(le) Calculator\n";
   int van;
   cout << "Enter Number: ";
   cin >> van;
 
   if (van == 1) {
-				cout << "Example of Sample C++, VLDZ";
+				cout << "VLDZ";
 			}
 
   else if (van == 2) {
-    int q1;
-    int q2;
-    int sum;
-    cout << "Enter First Number: ";
-    cin >> q1;
-    cout << "Enter Second Number: ";
-    cin >> q2;
+    int op;
+    int num1, num2;
 
-    sum = q1 + q2;
+    //answers
 
-    cout << "Combination is: " << sum << endl;
+    
+    cout << "Simp(le) Calculator" << endl;
+   cout << "Please type 1 to use Addition" << endl;
+    cout << "Please type 2 to use Subtraction" << endl;
+    cout << "Please type 3 to use Multiplication" << endl;
+
+     cout << "Enter Number: ";
+     cin >> op;
+
+    cout << "Please enter number 1: " << endl;
+    cin >>  num1;
+
+    cout << "Please enter number 2: " << endl;
+    cin >>  num2;
+
+    int suw = num1+num2;
+    int sub = num1-num2;
+    int mul = num1*num2;
+
+    if(op==1){
+      cout << "Combination is: " << suw<< endl;
+    }
+
+        else if(op==2){
+      cout << "Difference is: " << sub << endl;
+    }
+
+        else if(op==3){
+      cout << "Product is: " << mul << endl;
+    }
+
+    else if (op>=4){
+      cout << "Selected number is not even a function!";
+    }
+
     
 }
   }
